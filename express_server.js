14c2b41f79f5cb373452f1c3c8ce57c346a0b431
MarkZsombor <markszsombor@gmail.com>
2017-11-07 22:08:20 +0000

    first commit

---
+++ express_server.js
@@ -0,0 +1,36 @@
+var express = require("express");
+var app = express();
+var PORT = process.env.PORT || 8080; // default port 8080
+
+app.set("view engine", "ejs")
+
+var urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+app.get("/", (req, res) => {
+  res.end("Hello!");
+});
+
+app.get("/urls.json", (req, res) => {
+  res.json(urlDatabase);
+});
+
+app.get("/urls", (req, res) => {
+  let templateVars = { urls: urlDatabase };
+  res.render("urls_index", templateVars);
+});
+
+app.get("/urls/:id", (req, res) => {
+  let templateVars = { urls: urlDatabase, shortURL: req.params.id };
+  res.render("urls_show", templateVars);
+});
+
+app.get("/hello", (req, res) => {
+  res.end("<html><body>Hello <b>World</b></body></html>\n");
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
